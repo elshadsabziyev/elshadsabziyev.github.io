22de5383a49b71bb23f41fb99725b5206d350240
Elshad Sabziyev <sabziyevelshad@gmail.com>
2024-07-24 00:40:55 +0400

    chore: Exclude .vscode and update styles for sidebar and sections

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const expandableContents = document.querySelectorAll('.expandable-content');
-
     expandableContents.forEach(content => {
         const header = content;
         const hiddenContent = content.querySelector('.hidden-content');
         const expandIcon = content.querySelector('.expand-icon');
-
         header.addEventListener('click', function () {
             content.classList.toggle('expanded');
-
             if (content.classList.contains('expanded')) {
                 hiddenContent.style.maxHeight = hiddenContent.scrollHeight + "px";
                 if (expandIcon) expandIcon.textContent = 'expand_less';
